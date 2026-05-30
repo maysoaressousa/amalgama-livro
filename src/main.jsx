5e96a824947ebb958aaa306cb1e99e8d3bce498e
Mayara Soares <141755987+maydreadful@users.noreply.github.com>
2026-05-30 17:42:20 -0300

    fix: corrige importacao do css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './app.css' // 👈 ESSA LINHA PRECISA ESTAR AQUI!
+import './App.css' // 👈 ESSA LINHA PRECISA ESTAR AQUI!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
